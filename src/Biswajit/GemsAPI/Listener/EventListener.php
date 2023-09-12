c70972585a66d34ddd6debcd7e497665902016c0
Biswajit <121815367+GabBiswajit@users.noreply.github.com>
2023-09-12 22:39:21 +0530

    Update EventListener.php

--- src/Biswajit/GemsAPI/Listener/EventListener.php
+++ src/Biswajit/GemsAPI/Listener/EventListener.php
@@ -14,28 +14,21 @@ use pocketmine\Server;
 
 class EventListener implements Listener
 {
-  public function onJoin(PlayerJoinEvent $event)
-{
-    $player = $event->getPlayer();
-    $playerName = $player->getName();
-    $stmt = GemsAPI::getInstance()->database->prepare("INSERT OR IGNORE INTO player_data (player_name, gems) VALUES (:name, 0)");
-    $stmt->bindValue(":name", $playerName, SQLITE3_TEXT);
-    $stmt->execute();
-    $stmt->close();
-}
-
-public function onQuit(PlayerQuitEvent $event): void
-{
-    $player = $event->getPlayer();
-    $playerName = $player->getName();
-    if (isset($this->gemsMoney[$playerName])) {
-        $currentGems = $this->gemsMoney[$playerName];
-        $stmt = GemsAPI::getInstance()->database->prepare("UPDATE player_data SET gems = :gems WHERE player_name = :name");
-        $stmt->bindValue(":name", $playerName, SQLITE3_TEXT);
-        $stmt->bindValue(":gems", $currentGems, SQLITE3_FLOAT);
-        $stmt->execute();
-        $stmt->close();
-        unset($this->gemsMoney[$playerName]);
+    public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        if (!file_exists(GemsAPI::getInstance()->getDataFolder() . "Data/" . $player->getName() . ".yml")) {
+            new Config(GemsAPI::getInstance()->getDataFolder() . "Data/" . $player->getName() . ".yml", Config::YAML, array(
+                "Gems" => 0,
+            ));
+        }
     }
- }
+
+
+    public function onQuit(PlayerQuitEvent $event): void
+    {
+        $player = $event->getPlayer();
+        GemsAPI::getInstance()->saveData($player);
+        unset(GemsAPI::getInstance()->gemsMoney[$player->getName()]);
+    }
+
 }
